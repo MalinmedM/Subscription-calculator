73f0666d2f5e0214f681b64c885a904b014a5041
Malin Håkansson <malin.hson@gmail.com>
2023-07-06 16:41:14 +0200

    Change event listeners

--- js-lesson07-subscription-calculator/js/script.js
+++ js-lesson07-subscription-calculator/js/script.js
@@ -1,3 +1,17 @@
-var subTypeElement = document.querySelector(".subscription-select");
+// Selecting elements
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
 
-var SubDurationElement = document.querySelector(".duration-select");
+// Creating change event listeners
+subTypeElement.addEventListener("change", function(e) {
+    subType = e.target.value
+    //console.log(subType)
+});
+
+subDurationElement.addEventListener("change", function(e) {
+    subDuration = Number(e.target.value)
+    console.log(subDuration);
+});
