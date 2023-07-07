38fd5e3a862051a9c3c49b10ac0723acd715c86f
Malin Håkansson <malin.hson@gmail.com>
2023-07-07 20:58:23 +0200

    Function cost calculation #2

--- js-lesson07-subscription-calculator/js/script.js
+++ js-lesson07-subscription-calculator/js/script.js
@@ -6,24 +6,24 @@ var subType = "basic";
 var subDuration = 1;
 
 // Creating change event listeners
-subTypeElement.addEventListener("change", function(e) {
-    subType = e.target.value
-    //console.log(subType)
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  //console.log(subType)
 });
 
-subDurationElement.addEventListener("change", function(e) {
-    subDuration = Number(e.target.value)
-    //console.log(subDuration);
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  //console.log(subDuration);
 });
 
-// Function Cost calculation 
+// Function Cost calculation
 var updateSubscriptionDiv = function () {
-    var monthlyCost = 5;
-    if (subType === "standard") {
-        monthlyCost = 7
-    }  else if (subType === "premium") {
-            monthlyCost = 10;
-        }
-    var total = subDuration * monthlyCost;
-    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}`;
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}`;
 };
